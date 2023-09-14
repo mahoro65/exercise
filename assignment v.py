775a2fd72fb4ee99048b49d2a76ddd162153ed52
mahoro65 <143531273+mahoro65@users.noreply.github.com>
2023-09-14 05:49:37 -0700

    Add files via upload

---
+++ assignment v.py
@@ -0,0 +1,24 @@
+ #EXERCISE
+#change item value
+student_marks = ([90,85,75,69])
+student_marks[1] = ([89])
+print(student_marks)
+
+# #adding 55 to the list
+student_marks = [60,80,90,98]
+student_marks.append(55)
+print(student_marks)
+
+# #find the size of the list having appended 5
+print(len(student_marks))
+
+# #write a python program to sum all item in the list
+student_marks = (student_marks)
+
+
+#write a python function that takes two lists and retuns, if the have atleast one common member
+w = input('enter items')
+l = ('inter items')
+print('yes,there is a common number in w and l')
+
+     
